39eb6d8bf22e1896a810139ad749876a6d9ff7d1
aby913 <aby913@163.com>
2024-05-17 15:07:30 +0800

    fix: add os version support and verify apparmor and upgrade

--- build/installer/install_cmd.sh
+++ build/installer/install_cmd.sh
@@ -173,9 +173,10 @@ system_service_active() {
 precheck_os() {
     local ip os_type os_arch
 
-    # check os type and arch
+    # check os type and arch and os vesion
     os_type=$(uname -s)
     os_arch=$(uname -m)
+    os_verion=$(lsb_release -d 2>&1 | awk -F'\t' '{print $2}')
 
     if [ x"${os_type}" != x"Linux" ]; then
         log_fatal "unsupported os type '${os_type}', only supported 'Linux' operating system"
@@ -183,6 +184,10 @@ precheck_os() {
 
     if [[ x"${os_arch}" != x"x86_64" && x"${os_arch}" != x"amd64" ]]; then
         log_fatal "unsupported os arch '${os_arch}', only supported 'x86_64' architecture"
+    fi
+
+    if [[ $(is_ubuntu) -eq 0 && $(is_debian) -eq 0 ]]; then
+        log_fatal "unsupported os version '${os_verion}', only supported Ubuntu 20.x, 22.x, 24.x and Debian 11, 12"
     fi
 
     # try to resolv hostname
@@ -234,6 +239,59 @@ precheck_os() {
             ensure_success $sh_c "rm -rf /etc/resolv.conf.bak"
         fi
 
+    fi
+
+    # ubuntu 24 upgrade apparmor
+    ubuntuversion=$(is_ubuntu)
+    if [ ${ubuntuversion} -eq 2 ]; then
+        aapv=$(apparmor_parser --version)
+        if [[ ! ${aapv} =~ "4.0.1" ]]; then
+            ensure_success $sh_c "curl ${CURL_TRY} -k -sfLO https://launchpad.net/ubuntu/+source/apparmor/4.0.1-0ubuntu1/+build/28428840/+files/apparmor_4.0.1-0ubuntu1_amd64.deb"
+            ensure_success $sh_c "dpkg -i apparmor_4.0.1-0ubuntu1_amd64.deb"
+        fi
+    fi
+}
+
+is_debian() {
+    lsb_release=$(lsb_release -d 2>&1 | awk -F'\t' '{print $2}')
+    if [ -z "$lsb_release" ]; then
+        echo 0
+        return
+    fi
+    if [[ ${lsb_release} == *Debian*} ]]; then
+        case "$lsb_release" in
+            *12.* | *11.*)
+                echo 1
+                ;;
+            *)
+                echo 0
+                ;;
+        esac
+    else
+        echo 0
+    fi
+}
+
+is_ubuntu() {
+    lsb_release=$(lsb_release -d 2>&1 | awk -F'\t' '{print $2}')
+    if [ -z "$lsb_release" ]; then
+        echo 0
+        return
+    fi
+    if [[ ${lsb_release} == *Ubuntu* ]];then 
+        case "$lsb_release" in
+            *24.*)
+                echo 2
+                ;;
+            *22.* | *20.*)
+                echo 1
+                ;;
+            *)
+                echo 0
+                ;;
+        esac
+    else
+        echo 0
     fi
 }
 
