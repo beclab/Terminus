9d0045d9ac4ff32722d17839635488a80e74665b
aby913 <aby913@163.com>
2024-05-27 17:18:48 +0800

    fix: support offline installation packages for k8s

---
+++ scripts/download-deps.sh
@@ -0,0 +1,54 @@
+arch="amd64"
+part=""
+
+cat ./dependencies.mf | while IFS= read -r line; do
+    [[ -z "$line" ]] && continue
+
+    case "$line" in
+        *\[components\]*)
+            part="components"
+            mkdir -p "${part}"
+            continue
+            ;;
+        *\[pkg\]*)
+            part="pkg"
+            mkdir -p "${part}"
+            continue
+            ;;
+        *)
+            ;;
+    esac
+
+    if [ -z "$part" ]; then
+        exit 1
+    fi
+
+    s1=$(echo "$line" | cut -d',' -f1)
+    s2=$(echo "$line" | cut -d',' -f2)
+    s3=$(echo "$line" | cut -d',' -f3)
+
+    file=$(echo "$s1" | rev | cut -d'/' -f1 | rev)
+
+    if [ "$part" == "components" ]; then
+        if [ -z "$s2" ]; then
+            curl -L -o ./${part}/${file} ${s1}
+        else
+            curl -L -o ./${part}/${s2} ${s1}
+        fi
+    else
+        s4=$(echo "$line" | cut -d',' -f4)
+        pkgpath="./${part}/${s2}/${arch}"
+        mkdir -p ${pkgpath}
+        filename=${file}
+        if [ ! -z ${s3} ]; then
+            filename=${s3}
+        fi
+        curl -L -o ${pkgpath}/${filename} ${s1}
+
+        if [ "$s4" == "helm" ]; then
+            pushd ${pkgpath}
+            tar -zxvf ./${filename} && cp ./linux-amd64/helm ./ && rm -rf ./linux-amd64 && rm -rf ./${filename}
+            popd
+        fi
+    fi
+done
