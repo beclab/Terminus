cecd5754bd792a8e83351edb105c837b4537ae52
aby913 <aby913@163.com>
2024-05-30 14:50:20 +0800

    fix: pull velero image failed (#90)

--- build/installer/install_cmd.sh
+++ build/installer/install_cmd.sh
@@ -1071,8 +1071,8 @@ install_velero_plugin_terminus() {
   namespace="os-system"
   storage_location="terminus-cloud"
   bucket="terminus-cloud"
-  image="beclab/velero:v1.11.1"
-  plugin="beclab/velero-plugin-for-terminus:v1.0.2"
+  image="beclab/velero:v1.11.0"
+  plugin="beclab/velero-plugin-for-terminus:v1.0.1"
 
   if [[ "$provider" == x"" || "$namespace" == x"" || "$bucket" == x"" || "$image" == x"" || "$plugin" == x"" ]]; then
     echo "Backup plugin install params invalid."
