2071f06a83b6cff2d6bd0c9180fdbea94520cdcf
liuyu <>
2024-08-27 20:43:31 +0800

    fix: set -e

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -1,4 +1,3 @@
-set -ex
 set -o pipefail
 
 PLATFORM=${2:-linux/amd64}
@@ -14,6 +13,7 @@ cat $1|while read image; do
 
     curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$path$name.tar.gz > /dev/null
     if [ $? -ne 0 ]; then
+        set -e
         docker pull $image
         docker save $image -o $name.tar
         gzip $name.tar
@@ -23,11 +23,13 @@ cat $1|while read image; do
         aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
         aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
         echo "upload $name completed"
+        set +e
     fi
 
     # re-upload checksum.txt
     curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$path$checksum > /dev/null
     if [ $? -ne 0 ]; then
+        set -e
         docker pull $image
         docker save $image -o $name.tar
         gzip $name.tar
@@ -37,6 +39,7 @@ cat $1|while read image; do
         aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
         aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
         echo "upload $name completed"
+        set +e
     fi
 
 done
