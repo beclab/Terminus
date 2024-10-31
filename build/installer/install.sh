e6414f682e2803edcfac65088724f0c17e6a98f3
dkeven <82354774+dkeven@users.noreply.github.com>
2024-10-31 23:40:35 +0800

    fix: update CLI version to 0.1.38 (#592)

--- build/installer/install.sh
+++ build/installer/install.sh
@@ -62,7 +62,7 @@ if [ ! -d $BASE_DIR ]; then
     mkdir -p $BASE_DIR
 fi
 
-CLI_VERSION="0.1.37"
+CLI_VERSION="0.1.38"
 CLI_FILE="terminus-cli-v${CLI_VERSION}_linux_${ARCH}.tar.gz"
 if [[ x"$os_type" == x"Darwin" ]]; then
     CLI_FILE="terminus-cli-v${CLI_VERSION}_darwin_${ARCH}.tar.gz"
