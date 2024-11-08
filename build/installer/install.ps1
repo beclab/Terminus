8f92e164fcfed062c3943fdf351f449303a81e33
aby913 <aby913@163.com>
2024-11-08 18:33:02 +0800

    fix: run ps1 as admin

--- build/installer/install.ps1
+++ build/installer/install.ps1
@@ -2,20 +2,16 @@ $currentPath = Get-Location
 $architecture = $env:PROCESSOR_ARCHITECTURE
 $version = "#__VERSION__"
 
-function Test-IsAdmin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole("Administrators")
-}
-
-if (-Not (Test-IsAdmin)) {
-  Write-Host "Not running as Administrator"
-  exit 1
+function Test-Wait {
+  while ($true) {
+    Start-Sleep -Seconds 1
+  }
 }
 
 $process = Get-Process -Name olares-cli -ErrorAction SilentlyContinue
 if ($process) {
-  Write-Host "olares-cli.exe is running, exit."
-  exit 1
+  Write-Host "olares-cli.exe is running, Press Ctrl+C to exit."
+  Test-Wait
 }
 
 $arch = "amd64"
@@ -48,4 +44,4 @@ wsl --unregister Ubuntu *> $null
 Start-Sleep -Seconds 3
 $arguments = @("terminus", "install", "--version", $version)
 Write-Host ("Preparing to start the installation of Olares {0}. Depending on your network conditions, this process may take several minutes." -f $version)
-Start-Process -FilePath $cliPath -ArgumentList $arguments -Wait
+Start-Process -FilePath $cliPath -ArgumentList $arguments -Wait -Verb RunAs
