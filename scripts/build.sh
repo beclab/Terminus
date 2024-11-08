461f0339cc56dd130e238ecbc5e01815adc1a377
aby913 <aby913@163.com>
2024-11-08 14:37:55 +0800

    feat: windows install script

--- scripts/build.sh
+++ scripts/build.sh
@@ -37,6 +37,7 @@ fi
 if [ ! -z $VERSION ]; then
     sh -c "$SED 's/#__VERSION__/${VERSION}/' wizard/config/settings/templates/terminus_cr.yaml"
     sh -c "$SED 's/#__VERSION__/${VERSION}/' install.sh"
+    sh -c "$SED 's/#__VERSION__/${VERSION}/' install.ps1"
     VERSION="v${VERSION}"
 else
     VERSION="debug"
