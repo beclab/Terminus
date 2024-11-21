7f514564cb1eb050190d3d9e2058ecbf62a83e73
dkeven <82354774+dkeven@users.noreply.github.com>
2024-11-21 23:42:05 +0800

    fix: make systemd always restart redis (#651)

--- build/installer/install.sh
+++ build/installer/install.sh
@@ -67,7 +67,7 @@ if [ -z ${cdn_url} ]; then
     cdn_url="https://dc3p1870nn3cj.cloudfront.net"
 fi
 
-CLI_VERSION="0.1.58"
+CLI_VERSION="0.1.60"
 CLI_FILE="olares-cli-v${CLI_VERSION}_linux_${ARCH}.tar.gz"
 if [[ x"$os_type" == x"Darwin" ]]; then
     CLI_FILE="olares-cli-v${CLI_VERSION}_darwin_${ARCH}.tar.gz"
