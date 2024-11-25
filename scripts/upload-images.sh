4609729c029bf98d35a2ebaa0b2c944cfc5cc9af
liuyu <>
2024-11-25 17:29:31 +0800

    ci: upload images to cos

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -23,8 +23,11 @@ cat $1|while read image; do
         aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
         aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
         echo "upload $name completed"
+        
         set +e
     fi
+
+    
 
     # re-upload checksum.txt
     curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$path$checksum > /dev/null
@@ -41,5 +44,43 @@ cat $1|while read image; do
         echo "upload $name completed"
         set +e
     fi
+
+    # upload to tencent cloud cos
+
+    curl -fsSLI https://cdn.joinolares.cn/$path$name.tar.gz > /dev/null
+    if [ $? -ne 0 ]; then
+        set -e
+        docker pull $image
+        docker save $image -o $name.tar
+        gzip $name.tar
+
+        md5sum $name.tar.gz > $checksum
+
+        coscmd upload ./$name.tar.gz /$path$name.tar.gz
+        coscmd upload ./$checksum /$path$checksum
+        echo "upload $name to cos completed"
+
+        set +e
+    fi
+
+    
+
+    # re-upload checksum.txt
+    curl -fsSLI https://cdn.joinolares.cn/$path$checksum > /dev/null
+    if [ $? -ne 0 ]; then
+        set -e
+        docker pull $image
+        docker save $image -o $name.tar
+        gzip $name.tar
+
+        md5sum $name.tar.gz > $checksum
+
+        coscmd upload ./$name.tar.gz /$path$name.tar.gz
+        coscmd upload ./$checksum /$path$checksum
+        echo "upload $name to cos completed"
+
+        set +e
+    fi
+    
 
 done
