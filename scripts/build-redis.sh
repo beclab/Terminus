709e7d195dd6f78295731040f05565d25edab2e5
eball <liuy102@hotmail.com>
2024-12-04 17:15:38 +0800

    ci: build redis for glibc_2.31 (#702)
    Co-authored-by: liuyu <>

--- scripts/build-redis.sh
+++ scripts/build-redis.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 PLATFORM=${1:-linux/amd64}
+SUFFIX=$2
+
+if [[ "$SUFFIX" != "" ]]; then
+    SUFFIX="-${SUFFIX}"
+fi
+
 
 os=$(echo "$PLATFORM"|awk -F"/" '{print $1}')
 arch=$(echo "$PLATFORM"|awk -F"/" '{print $2}')
@@ -21,4 +27,4 @@ mkdir redis-5.0.14 && \
 cp /usr/local/bin/redis* ./redis-5.0.14/
 
 tar czvf ./redis-5.0.14.tar.gz ./redis-5.0.14/ && \
-aws s3 cp redis-5.0.14.tar.gz s3://terminus-os-install/redis-5.0.14_${os}_${arch}.tar.gz --acl=public-read
+aws s3 cp redis-5.0.14.tar.gz s3://terminus-os-install/redis-5.0.14_${os}_${arch}${SUFFIX}.tar.gz --acl=public-read
